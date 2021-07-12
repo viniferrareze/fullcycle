03fe5339b7a36b89c021e60568ec1146f42a5d9d
Carlos Vinícius Ferrareze <viniferrareze@gmail.com>
2021-07-12 16:45:57 -0300

    first commit

---
+++ codebank/infrastructure/repository/transaction.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/viniferrareze/fullcycle/codebank/domain"
+)
+
+type TransactionRepositoryDb struct {
+	db *sql.DB
+}
+
+func NewTransactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
+	return &TransactionRepositoryDb{db: db}
+}
+
+func (t *TransactionRepositoryDb) SaveTransaction(transaction domain.Transaction, creditCard domain.CreditCard) error {
+	stmt, err := t.db.Prepare( `insert into transactions(id, credit_card_id, amount, status, description, store, created_at)
+								values($1, $2, $3, $4, $5, $6, $7)`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(
+		transaction.ID,
+		transaction.CreditCardId,
+		transaction.Amount,
+		transaction.Status,
+		transaction.Description,
+		transaction.Store,
+		transaction.CreatedAt, 
+	)
+	if err != nil {
+		return err
+	}
+	if transaction.Status == "approved" {
+		err = t.updateBalance(creditCard)
+		if err != nil {
+			return err
+		}
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
